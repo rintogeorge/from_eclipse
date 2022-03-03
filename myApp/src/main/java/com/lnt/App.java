d2f76f77b8f5e598a9ffe57aded2443d971f34b4
rinto george <rintogeorge5@gmail.com>
2022-03-03 14:07:43 +0530

    changed

--- myApp/src/main/java/com/lnt/App.java
+++ myApp/src/main/java/com/lnt/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World it is from eclipse" );
+        System.out.println("Program ended");
     }
 }
